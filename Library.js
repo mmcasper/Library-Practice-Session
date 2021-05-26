022a260049cb9e94b4e7bb6069304afc44a7e6c5
Meghan Casper <casper.sriracha@gmail.com>
2021-05-26 13:01:43 -0400

    first commit

---
+++ Library.js
@@ -0,0 +1,19 @@
+class Library{
+    constructor(books, patron) {
+        this.books = books;
+        this.patron = patron;
+    }
+
+    method addBook() {
+
+
+    }
+
+    function addPatron() {
+
+
+    }
+
+
+
+}
---
+++ Patron.js
@@ -0,0 +1,17 @@
+class Patron{
+    constructor(name, email, currentBook){
+        this.name = name;
+        this.email = email;
+        this.currentBook = currentBook;
+    }
+
+    function checkOut() {
+
+
+    }
+
+    function returnBook() {
+
+    }
+
+}
